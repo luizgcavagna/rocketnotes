11d0438591a90aebebfa54bc5f14c18467a5cdca
Luiz G Cavagna <luizcavagna@gmail.com>
2025-01-06 22:50:37 -0300

    hash password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,32 @@
+const { hash, Compare } = require('bcryptjs');
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
 
-   create(request, response) {
-      const { name, email } = request.body;
+   async create(request, response) {
+      const { name, email, password } = request.body;
 
       if(!name) 
          throw new AppError('Name is required', 400);
 
       if(!email)
          throw new AppError('Email is required', 400);
+
+      if(!password)
+         throw new AppError('Password is required', 400);
       
-      response.send(`${email} ${name}`);
+      const database = await sqliteConnection();
+      const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+      if(checkUserExists)
+         throw new AppError('User already exists');
+
+      const hashedPassword = await hash(password, 8);
+
+      await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
+
+      response.send(`User ${name}, created successfully!`);
    }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,6 @@ const UsersController = require('../controllers/UsersController');
 const usersRoutes = Router();
 const usersController = new UsersController();
 
-
-
 usersRoutes.post('/create', usersController.create);
 
 module.exports = usersRoutes;
